d127407bb060733ce1f676c55d93120c49094858
Irongade <adeoluayangade@yahoo.com>
2021-09-01 20:26:32 +0100

    Mini Vuejs project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <the-header title="Remember me"></the-header>
+  <!-- <stored-resources :resources="storedResources" ></stored-resources> -->
+  <the-resources></the-resources>
+</template>
+
+<script>
+// import StoredResources from './components/learning-resource/StoredResources.vue'
+import TheHeader from './components/layouts//TheHeader.vue';
+import TheResources from "./components/learning-resource/TheResources.vue";
+
+export default {
+    components: {
+        // StoredResources,
+        TheHeader,
+        TheResources
+    },
+}
+
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+</style>
